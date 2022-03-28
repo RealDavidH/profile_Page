ebe070c90bfd4aa0c6aa1cd9ff69bd6be977befc
David Hollinger <david.hollinger123@gmail.com>
2022-03-28 13:51:42 -0700

    Most buttons on the page now do things

--- js_Library.js
+++ js_Library.js
@@ -1,105 +1,67 @@
-//!!!!!!COOKIE POP UP FUNCTION!!!!!!
-//New popup code
+let count = 2;
+let count2 = 420;
+let user = document.querySelector(".Jane")
 
-function closeCookie() {
-    let main = document.querySelector("#main");
-    let checkbox = document.querySelector("#cookie_check_Box");
-    let alert2 = document.querySelector("#hidden_alert");
-    if (checkbox.checked == true) {
-        document.getElementById("cookie-popup").style.display = "none";
-        main.classList.remove("blur");
-    } else {
-        // alert("YOU MUST AGREE TO GIVE US YOUR SOUL!")
-        alert2.style.display = "block";
-    }
+let friends = {
+    "Phil": document.querySelector(".friend"),
+    "Todd": document.querySelector(".friend2")
+}
+let buttons = {
+    "accept_btn": document.querySelector(".friend_button"),
+    "decline_btn": document.querySelector(".friend_button"),
+    "accept_btn2": document.querySelector(".friend_button2"),
+    "decline_btn2": document.querySelector(".friend_button2"),
 }
 
-// When the user clicks on div, open the popup
-// older code
-// function open() {
-//   document.getElementById("myForm").style.display = "block";
-// }
-// function close() {
-//   document.getElementById("myForm").style.display = "none";
-// }
-//      If you want a text to display when you check the check mark. Make this function not a comment and the html on line 87
-//     function checkBox() {
-//         let checkBox = document.getElementById("cookie_check_Box");
-//         let text = document.getElementById("text");
-//         if (checkBox.checked == true) {
-//             text.style.display = "block";
-//         } else {
-//             text.style.display = "none";
-//         }
-//     }
-//!!!!!!COOKIE POP UP FUNCTION!!!!!!
-
-//!!!!!! Button even lister, that when you click on button the number of likes goes up by 1.!!!!!!!!!!!!!!!!
-let btn_Yellow = document.querySelector(".btn_Yellow");
-function addEvent() {
-    btn_Yellow.addEventListener("click", changeLike());
-}
-
-let like = 0;
-function changeLike() {
-    like++; // makes the buttons number value go up by one, every time this function is run
-    alert("You have liked this!"); //Alerts user that the have clicked the like button
-    btn_Yellow.value = "likes: " + like;
-}
-
-// If you for some reason, like me, have two yellow buttons you use two of these functions like so
-// let btn_Yellow2 = document.querySelector(".btn_Yellow2");
-// function addEvent2(){
-//     btn_Yellow2.addEventListener("click", changeLike2());
-// }
-
-// let like2 = 0;
-//     function changeLike2(){
-//         like++;
-//             btn_Yellow2.value = "likes: "+like;
-// }
-//!!!!!! Button event lister!!!!!!!!!
-
-//Changes a button from login to logout
-let btn_Normal = document.querySelector(".btn");
-function login() {
-    if (btn_Normal.value == "Login") {
-        btn_Normal.value = "Log out";
-    } else btn_Normal.value = "Login";
-}
-
-//Dark mode button
-//!!!REQUIRES BOOTSTRAP ICONS TO FUNCTION!!! 
-let container = document.querySelector(".container")
-let body = document.querySelector("body");
-let moon = document.querySelector(".btn_moon");
-let notDark = false; //keeps track of if night mode is on or not
-
-function dark(){
-    if (notDark){
-        //Changes container and body to include class night.
-        container.classList.add("night"); //doesn't work on container because it has a background color already
-        body.classList.add("night");
-        //removing the first moon icon
-        moon.classList.remove("bi");
-        moon.classList.remove("bi-moon");
-        //adding a white background and adding new moon icon 
-        moon.classList.add("white")
-        moon.classList.add("bi-moon-fill");
-        moon.classList.add("bi");
-        notDark = false;
-
-    } else{
-        //removes class night from container and body.
-        container.classList.remove("night"); //doesn't work on container because it has a background color already
-        body.classList.remove("night");
-        moon.classList.remove("bi");
-        //removing second moon icon and removing class white to make it a black icon
-        moon.classList.remove("bi-moon-fill");
-        moon.classList.remove("white")
-        //adding OG Icon back
-        moon.classList.add("bi-moon");
-        moon.classList.add("bi");
-        notDark = true;
-    }
+let connectVal ={
+    "connReq": document.querySelector(".btn_round"),
+    "urConnct": document.querySelector(".btn_round2"),
 } 
+
+
+function accept(){
+    friends.Phil.classList.add("hidden");
+    buttons.accept_btn.classList.add("hidden");
+    buttons.decline_btn.classList.add("hidden");
+}
+
+function accept2(){
+    friends.Todd.classList.add("hidden");
+    buttons.accept_btn2.classList.add("hidden");
+    buttons.decline_btn2.classList.add("hidden");
+}
+
+function update_decline(){
+    count--
+    count2 -= Math.floor(Math.random()*100)+1
+    connectVal.urConnct.innerHTML = count2;
+    connectVal.connReq.innerHTML = count;
+}
+
+function update_accept(){
+    count--
+    count2 += Math.floor(Math.random()*100)+1
+    if(count2 >= 500){
+        connectVal.urConnct.innerHTML = "500+";
+        connectVal.connReq.innerHTML = count;
+    }else {
+    connectVal.urConnct.innerHTML = count2;
+    connectVal.connReq.innerHTML = count;
+    }
+}
+function randomName(){
+    let names = [
+        "Billie-Rae Digiqiat",
+        "Naidlent Lord Zarak",
+        "Sottick Nerposh",
+        "Sheppulk Starfriend",
+        "Qaggum Moffant",
+    ]
+    return names[Math.floor(Math.random()*names.length-1)+1];
+}
+
+function edit_profile(){
+    user.innerHTML = randomName();
+}
+
+
